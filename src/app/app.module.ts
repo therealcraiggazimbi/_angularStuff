7d4a4ce2b4cd6f28217255e781ef2ed61aaab85f
therealcraiggazimbi <craiggazimbi@gmail.com>
2023-02-14 09:05:47 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [AppComponent, ServersComponent, ServerComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
+})
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server created';
+  serverName = '';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = `Server was created ${this.serverName}`;
+  }
+
+  onUpdateServeName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
